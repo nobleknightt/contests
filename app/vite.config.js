6edeadd3ab7a81a5cb037d48bac746e7f197c2eb
er-knight <60701544+er-knight@users.noreply.github.com>
2024-02-13 23:56:13 +0530

    made pwa using vite-pwa-plugin

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        "name": "Contests",
+        "short_name": "Contests",
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#FFFFFF",
+        "theme_color": "#FFFFFF",
+        "description": "Ongoing and Upcoming Contests on AtCoder, CodeChef, Codeforces, GeeksforGeeks and LeetCode"
+      }
+    })
+  ],
   base: './'
 })
